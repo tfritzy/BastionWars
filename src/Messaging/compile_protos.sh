2fe5051005b545579f4836a1fc21d29b723c4a8a
Tyler <tfritzy@gmail.com>
2024-06-20 21:48:19 -0700

    Starting on messaging

---
+++ src/Messaging/compile_protos.sh
@@ -0,0 +1,11 @@
+CS_FILE="Schema.cs"
+
+echo "Generating c#"
+npx protoc -I=. --csharp_out="." schema.proto
+
+if [ -f "$CS_FILE" ]; then
+  sed -i 's/1591, 0612, 3021/1591, 0612, 3021, 8600, 8981/g' "$CS_FILE"
+  echo "Replaced text in $CS_FILE"
+else
+  echo "Warning: $CS_FILE not found. Skipping text replacement."
+fi
