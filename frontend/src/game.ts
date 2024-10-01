3e99acb9c2ebe43e408b33b5f3aaad0e700d14c4
Tyler <tfritzy@gmail.com>
2024-09-30 20:08:08 -0500

    Wind

--- frontend/src/game.ts
+++ frontend/src/game.ts
@@ -1,304 +1,273 @@
 import { Connection } from "./connection.ts";
 import {
- ARROW_COLOR,
- ARROW_LENGTH,
- KEEP_LABEL_FONT,
- KEEP_LABEL_STROKE,
- Layer,
- UNIT_COLOR,
- UNIT_OUTLINE_COLOR,
- UNIT_OUTLINE_WIDTH,
- UNIT_RADIUS,
- WORLD_TO_CANVAS,
+  ARROW_COLOR,
+  ARROW_LENGTH,
+  KEEP_LABEL_FONT,
+  KEEP_LABEL_STROKE,
+  Layer,
+  UNIT_COLOR,
+  UNIT_OUTLINE_COLOR,
+  UNIT_OUTLINE_WIDTH,
+  UNIT_RADIUS,
+  WORLD_TO_CANVAS,
 } from "./constants.ts";
 import { Drawing } from "./drawing.ts";
 import { drawMap } from "./grid_drawing.ts";
 import { drawKeep } from "./keep_drawing.ts";
 import {
- TileType,
- type AllKeepUpdates,
- type AllSoldierPositions,
- type GameFoundForPlayer,
- type InitialState,
- type NewProjectiles,
- type Oneof_GameServerToPlayer,
- type RenderTileUpdates,
+  TileType,
+  type AllKeepUpdates,
+  type AllSoldierPositions,
+  type GameFoundForPlayer,
+  type InitialState,
+  type NewProjectiles,
+  type Oneof_GameServerToPlayer,
+  type RenderTileUpdates,
 } from "./Schema.ts";
-import { drawTree, drawTreeLayer } from "./tree_drawing.ts";
+import { drawTree } from "./tree_drawing.ts";
 import { Typeable } from "./typeable.ts";
 import {
- addV3,
- cloneMultiplyV3,
- divide,
- initialGameState,
- magnitude,
- parseKeep,
- parseProjectile,
- parseSoldier,
- updateSoldier,
- type GameState,
+  addV3,
+  cloneMultiplyV3,
+  divide,
+  initialGameState,
+  magnitude,
+  parseKeep,
+  parseProjectile,
+  parseSoldier,
+  updateSoldier,
+  type GameState,
 } from "./types.ts";
 
 export class Game {
- private canvas: HTMLCanvasElement;
- private ctx: CanvasRenderingContext2D;
- private connection: Connection | undefined;
- private gameState: GameState = initialGameState;
- private keepLabels: Map<number, Typeable> = new Map();
- private selectedKeep: number | null = null;
- private drawing: Drawing;
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+  private connection: Connection | undefined;
+  private gameState: GameState = initialGameState;
+  private keepLabels: Map<number, Typeable> = new Map();
+  private selectedKeep: number | null = null;
+  private drawing: Drawing;
+  private time: number = 0;
 
- constructor(
-  canvas: HTMLCanvasElement,
-  ctx: CanvasRenderingContext2D
- ) {
-  this.canvas = canvas;
-  this.ctx = ctx;
-  this.drawing = new Drawing();
- }
+  constructor(canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.drawing = new Drawing();
+  }
 
- draw(dpr: number, deltaTime: number): void {
-  drawMap(this.drawing, this.gameState);
-  this.drawKeeps(deltaTime);
-  this.drawSoldiers();
-  this.drawing.draw(this.ctx);
-  this.drawKeepLables(deltaTime);
-  this.drawProjectiles(deltaTime);
-  this.drawTrees(deltaTime);
- }
+  draw(dpr: number, deltaTime: number): void {
+    this.time += deltaTime;
+    drawMap(this.drawing, this.gameState);
+    this.drawKeeps(deltaTime);
+    this.drawSoldiers();
+    this.drawing.draw(this.ctx);
+    // this.drawKeepLables(deltaTime);
+    this.drawProjectiles(deltaTime);
+    this.drawTrees(this.time);
+  }
 
- drawKeeps(deltaTime: number) {
-  this.gameState.keeps.forEach((keep) => {
-   const x = keep.pos.x * WORLD_TO_CANVAS;
-   const y = keep.pos.y * WORLD_TO_CANVAS;
-   drawKeep(this.drawing, keep, deltaTime);
-  });
- }
+  drawKeeps(deltaTime: number) {
+    this.gameState.keeps.forEach((keep) => {
+      const x = keep.pos.x * WORLD_TO_CANVAS;
+      const y = keep.pos.y * WORLD_TO_CANVAS;
+      drawKeep(this.drawing, keep, deltaTime);
+    });
+  }
 
- drawTrees(deltaTime: number) {
-  this.gameState.tiles.forEach((tile, i) => {
-   if (tile == TileType.Tree) {
-    const x = Math.floor(i % this.gameState.mapWidth);
-    const y = Math.floor(i / this.gameState.mapWidth);
-    drawTree(this.drawing, x, y, deltaTime);
-   }
-  });
- }
+  drawTrees(time: number) {
+    this.gameState.tiles.forEach((tile, i) => {
+      if (tile == TileType.Tree) {
+        const x = Math.floor(i % this.gameState.mapWidth);
+        const y = Math.floor(i / this.gameState.mapWidth);
+        drawTree(this.drawing, x, y, time);
+      }
+    });
+  }
 
- drawKeepLables(deltaTime: number) {
-  this.gameState.keeps.forEach((keep) => {
-   const x = keep.pos.x * WORLD_TO_CANVAS;
-   const y = keep.pos.y * WORLD_TO_CANVAS;
-   this.keepLabels.get(keep.id)?.draw(x, y - 25, deltaTime);
-  });
- }
+  drawKeepLables(deltaTime: number) {
+    this.gameState.keeps.forEach((keep) => {
+      const x = keep.pos.x * WORLD_TO_CANVAS;
+      const y = keep.pos.y * WORLD_TO_CANVAS;
+      this.keepLabels.get(keep.id)?.draw(x, y - 25, deltaTime);
+    });
+  }
 
- soldierStyle = {
-  layer: Layer.Units,
-  fill_style: UNIT_COLOR,
-  line_width: UNIT_OUTLINE_WIDTH,
-  should_fill: true,
-  should_stroke: false,
-  stroke_style: UNIT_OUTLINE_COLOR,
- };
- drawSoldiers() {
-  this.gameState.soldiers.forEach((soldier) => {
-   this.drawing.drawCustom(this.soldierStyle, (ctx) => {
-    const x = soldier.pos.x * WORLD_TO_CANVAS;
-    const y = soldier.pos.y * WORLD_TO_CANVAS;
+  soldierStyle = {
+    layer: Layer.Units,
+    fill_style: UNIT_COLOR,
+    line_width: UNIT_OUTLINE_WIDTH,
+    should_fill: true,
+    should_stroke: false,
+    stroke_style: UNIT_OUTLINE_COLOR,
+  };
+  drawSoldiers() {
+    this.gameState.soldiers.forEach((soldier) => {
+      this.drawing.drawCustom(this.soldierStyle, (ctx) => {
+        const x = soldier.pos.x * WORLD_TO_CANVAS;
+        const y = soldier.pos.y * WORLD_TO_CANVAS;
 
-    ctx.moveTo(x + UNIT_RADIUS, y);
-    ctx.arc(x, y, UNIT_RADIUS, 0, 2 * Math.PI);
-   });
-  });
- }
+        ctx.moveTo(x + UNIT_RADIUS, y);
+        ctx.arc(x, y, UNIT_RADIUS, 0, 2 * Math.PI);
+      });
+    });
+  }
 
- drawProjectiles(deltaTime: number) {
-  for (
-   let i = this.gameState.projectiles.length - 1;
-   i >= 0;
-   i--
-  ) {
-   const proj = this.gameState.projectiles[i];
-   proj.remaining_life -= deltaTime;
-   proj.remaining_movement_time -= deltaTime;
+  drawProjectiles(deltaTime: number) {
+    for (let i = this.gameState.projectiles.length - 1; i >= 0; i--) {
+      const proj = this.gameState.projectiles[i];
+      proj.remaining_life -= deltaTime;
+      proj.remaining_movement_time -= deltaTime;
 
-   if (proj.remaining_life <= 0) {
-    const lastIndex = this.gameState.projectiles.length - 1;
-    if (i !== lastIndex) {
-     this.gameState.projectiles[i] =
-      this.gameState.projectiles[lastIndex];
+      if (proj.remaining_life <= 0) {
+        const lastIndex = this.gameState.projectiles.length - 1;
+        if (i !== lastIndex) {
+          this.gameState.projectiles[i] = this.gameState.projectiles[lastIndex];
+        }
+        this.gameState.projectiles.pop();
+        continue;
+      }
+
+      const hasLanded = proj.remaining_movement_time <= 0;
+      if (!hasLanded) {
+        proj.current_velocity.z -= proj.gravitational_force * deltaTime;
+        const deltaV = cloneMultiplyV3(proj.current_velocity, deltaTime);
+        addV3(proj.current_pos, deltaV);
+      }
+
+      const layer = hasLanded ? Layer.ProjectilesOnGround : Layer.Projectiles;
+      this.drawing.drawStrokeable(ARROW_COLOR, 1, layer, (ctx) => {
+        const x = proj.current_pos.x * WORLD_TO_CANVAS;
+        const y = proj.current_pos.y * WORLD_TO_CANVAS;
+        const xy = {
+          x: proj.current_velocity.x,
+          y: proj.current_velocity.y,
+        };
+        const xyMag = magnitude(xy);
+        const theta = Math.atan(proj.current_velocity.z / xyMag);
+        const arrowVisibleLength = Math.cos(theta) * ARROW_LENGTH;
+        const halfArrow = arrowVisibleLength / 2;
+        divide(xy, xyMag);
+
+        ctx.moveTo(x + xy.x * halfArrow, y + xy.y * halfArrow);
+        ctx.lineTo(x - xy.x * halfArrow, y - xy.y * halfArrow);
+      });
     }
-    this.gameState.projectiles.pop();
-    continue;
-   }
+  }
 
-   const hasLanded = proj.remaining_movement_time <= 0;
-   if (!hasLanded) {
-    proj.current_velocity.z -=
-     proj.gravitational_force * deltaTime;
-    const deltaV = cloneMultiplyV3(
-     proj.current_velocity,
-     deltaTime
+  connectToGameServer(details: GameFoundForPlayer) {
+    this.connection = new Connection(
+      details.address!,
+      details.player_id!,
+      details.auth_token!,
+      this.onMessage
     );
-    addV3(proj.current_pos, deltaV);
-   }
+  }
 
-   const layer = hasLanded
-    ? Layer.ProjectilesOnGround
-    : Layer.Projectiles;
-   this.drawing.drawStrokeable(
-    ARROW_COLOR,
-    1,
-    layer,
-    (ctx) => {
-     const x = proj.current_pos.x * WORLD_TO_CANVAS;
-     const y = proj.current_pos.y * WORLD_TO_CANVAS;
-     const xy = {
-      x: proj.current_velocity.x,
-      y: proj.current_velocity.y,
-     };
-     const xyMag = magnitude(xy);
-     const theta = Math.atan(
-      proj.current_velocity.z / xyMag
-     );
-     const arrowVisibleLength =
-      Math.cos(theta) * ARROW_LENGTH;
-     const halfArrow = arrowVisibleLength / 2;
-     divide(xy, xyMag);
-
-     ctx.moveTo(x + xy.x * halfArrow, y + xy.y * halfArrow);
-     ctx.lineTo(x - xy.x * halfArrow, y - xy.y * halfArrow);
+  handleTypeKeepName = (id: number) => {
+    if (this.selectedKeep == null) {
+      this.selectedKeep = id;
+    } else {
+      this.connection?.sendMessage({
+        sender_id: "Something I guess",
+        issue_deployment_order: {
+          source_keep: this.selectedKeep,
+          target_keep: id,
+        },
+      });
+      this.selectedKeep = null;
     }
-   );
-  }
- }
+  };
 
- connectToGameServer(details: GameFoundForPlayer) {
-  this.connection = new Connection(
-   details.address!,
-   details.player_id!,
-   details.auth_token!,
-   this.onMessage
-  );
- }
-
- handleTypeKeepName = (id: number) => {
-  if (this.selectedKeep == null) {
-   this.selectedKeep = id;
-  } else {
-   this.connection?.sendMessage({
-    sender_id: "Something I guess",
-    issue_deployment_order: {
-     source_keep: this.selectedKeep,
-     target_keep: id,
-    },
-   });
-   this.selectedKeep = null;
-  }
- };
-
- onMessage = (message: Oneof_GameServerToPlayer) => {
-  if (message.initial_state) {
-   this.handleInitialStateMsg(message.initial_state);
-  } else if (message.all_soldier_positions) {
-   this.handleSoldierPositionsMsg(
-    message.all_soldier_positions
-   );
-  } else if (message.keep_updates) {
-   this.handleKeepUpdates(message.keep_updates);
-  } else if (message.new_projectiles) {
-   this.handleNewProjectiles(message.new_projectiles);
-  } else if (message.render_tile_updates) {
-   this.handleRenderTileUpdates(
-    message.render_tile_updates
-   );
-  }
- };
-
- handleInitialStateMsg(msg: InitialState) {
-  console.log("initial state", msg);
-  msg.keeps?.forEach((k) => {
-   var keep = parseKeep(k);
-   if (keep != null) {
-    this.gameState.keeps.push(keep);
-    const id = keep.id;
-    this.keepLabels.set(
-     keep.id,
-     new Typeable(
-      keep.name,
-      KEEP_LABEL_FONT,
-      () => this.handleTypeKeepName(id),
-      this.drawing,
-      KEEP_LABEL_STROKE
-     )
-    );
-   }
-  });
-
-  this.gameState.tiles = msg.tiles || [];
-  this.gameState.renderTiles = msg.render_tiles || [];
-  this.gameState.mapHeight = msg.map_height || 0;
-  this.gameState.mapWidth = msg.map_width || 0;
- }
-
- handleSoldierPositionsMsg(msg: AllSoldierPositions) {
-  const currentTime = Date.now();
-  msg.soldier_positions?.forEach((s) => {
-   const existingSoldier =
-    s.id && this.gameState.soldiers.get(s.id);
-   if (!existingSoldier) {
-    const soldier = parseSoldier(s, currentTime);
-    if (soldier) {
-     this.gameState.soldiers.set(soldier.id, soldier);
+  onMessage = (message: Oneof_GameServerToPlayer) => {
+    if (message.initial_state) {
+      this.handleInitialStateMsg(message.initial_state);
+    } else if (message.all_soldier_positions) {
+      this.handleSoldierPositionsMsg(message.all_soldier_positions);
+    } else if (message.keep_updates) {
+      this.handleKeepUpdates(message.keep_updates);
+    } else if (message.new_projectiles) {
+      this.handleNewProjectiles(message.new_projectiles);
+    } else if (message.render_tile_updates) {
+      this.handleRenderTileUpdates(message.render_tile_updates);
     }
-   } else {
-    updateSoldier(existingSoldier, s, currentTime);
-   }
-  });
+  };
 
-  for (let [key, value] of this.gameState.soldiers) {
-   if (value.lastUpdated !== currentTime) {
-    this.gameState.soldiers.delete(key);
-   }
+  handleInitialStateMsg(msg: InitialState) {
+    console.log("initial state", msg);
+    msg.keeps?.forEach((k) => {
+      var keep = parseKeep(k);
+      if (keep != null) {
+        this.gameState.keeps.push(keep);
+        const id = keep.id;
+        this.keepLabels.set(
+          keep.id,
+          new Typeable(
+            keep.name,
+            KEEP_LABEL_FONT,
+            () => this.handleTypeKeepName(id),
+            this.drawing,
+            KEEP_LABEL_STROKE
+          )
+        );
+      }
+    });
+
+    this.gameState.tiles = msg.tiles || [];
+    this.gameState.renderTiles = msg.render_tiles || [];
+    this.gameState.mapHeight = msg.map_height || 0;
+    this.gameState.mapWidth = msg.map_width || 0;
   }
- }
 
- handleKeepUpdates(msg: AllKeepUpdates) {
-  msg.keep_updates?.forEach((ku) => {
-   const keepIndex = this.gameState.keeps.findIndex(
-    (k) => k.id === ku.id
-   );
-   if (keepIndex >= 0) {
-    this.gameState.keeps[keepIndex].archer_count =
-     ku.archer_count || 0;
-    this.gameState.keeps[keepIndex].warrior_count =
-     ku.warrior_count || 0;
-    this.gameState.keeps[keepIndex].alliance =
-     ku.alliance || 0;
-   }
-  });
- }
+  handleSoldierPositionsMsg(msg: AllSoldierPositions) {
+    const currentTime = Date.now();
+    msg.soldier_positions?.forEach((s) => {
+      const existingSoldier = s.id && this.gameState.soldiers.get(s.id);
+      if (!existingSoldier) {
+        const soldier = parseSoldier(s, currentTime);
+        if (soldier) {
+          this.gameState.soldiers.set(soldier.id, soldier);
+        }
+      } else {
+        updateSoldier(existingSoldier, s, currentTime);
+      }
+    });
 
- handleNewProjectiles(msg: NewProjectiles) {
-  msg.projectiles?.forEach((p) => {
-   const proj = parseProjectile(p);
-   if (proj) {
-    this.gameState.projectiles.push(proj);
-   }
-  });
- }
+    for (let [key, value] of this.gameState.soldiers) {
+      if (value.lastUpdated !== currentTime) {
+        this.gameState.soldiers.delete(key);
+      }
+    }
+  }
 
- handleRenderTileUpdates(msg: RenderTileUpdates) {
-  msg.render_tile_updates?.forEach((rt) => {
-   if (!rt || !rt.pos || !rt.render_tile) {
-    return;
-   }
+  handleKeepUpdates(msg: AllKeepUpdates) {
+    msg.keep_updates?.forEach((ku) => {
+      const keepIndex = this.gameState.keeps.findIndex((k) => k.id === ku.id);
+      if (keepIndex >= 0) {
+        this.gameState.keeps[keepIndex].archer_count = ku.archer_count || 0;
+        this.gameState.keeps[keepIndex].warrior_count = ku.warrior_count || 0;
+        this.gameState.keeps[keepIndex].alliance = ku.alliance || 0;
+      }
+    });
+  }
 
-   const index =
-    (rt.pos.x || 0) +
-    (rt.pos.y || 0) * (this.gameState.mapWidth + 1);
-   this.gameState.renderTiles[index] = rt.render_tile;
-  });
- }
+  handleNewProjectiles(msg: NewProjectiles) {
+    msg.projectiles?.forEach((p) => {
+      const proj = parseProjectile(p);
+      if (proj) {
+        this.gameState.projectiles.push(proj);
+      }
+    });
+  }
+
+  handleRenderTileUpdates(msg: RenderTileUpdates) {
+    msg.render_tile_updates?.forEach((rt) => {
+      if (!rt || !rt.pos || !rt.render_tile) {
+        return;
+      }
+
+      const index =
+        (rt.pos.x || 0) + (rt.pos.y || 0) * (this.gameState.mapWidth + 1);
+      this.gameState.renderTiles[index] = rt.render_tile;
+    });
+  }
 }
--- frontend/src/tree_drawing.ts
+++ frontend/src/tree_drawing.ts
@@ -1,133 +1,158 @@
-import {
- HALF_T,
- Layer,
- SHADOW_COLOR,
- TILE_SIZE,
- WORLD_TO_CANVAS,
-} from "./constants";
-import { drawCircle, type Drawing, type DrawStyle } from "./drawing";
-import { seededRandom } from "./random";
-import { addV3, type Vector2 } from "./types";
+import { HALF_T, Layer, SHADOW_COLOR, TILE_SIZE } from "./constants";
+import { type Drawing, type DrawStyle } from "./drawing";
+import { type Vector2 } from "./types";
 
-const NUM_BUMPS = [9, 7, 5];
-const RADIUS = [20, 14, 9];
-const BUMP_R = [8, 7, 5.8];
+const SHADOW_OFFSET = 3;
 
-const treeStyle: DrawStyle = {
- layer: Layer.TreeBottoms,
- fill_style: "white",
- line_width: 0.5,
- should_stroke: true,
- should_fill: true,
- stroke_style: "black",
+const treeLayerConfigs = [
+  { numBumps: 9, radius: 20, bumpRadius: 8 },
+  { numBumps: 7, radius: 12, bumpRadius: 6 },
+] as const;
+
+const createTreeStyle = (layer: number): DrawStyle => ({
+  layer,
+  fill_style: "white",
+  line_width: 0.5,
+  should_stroke: true,
+  should_fill: true,
+  stroke_style: "black",
+});
+
+const createShadowStyle = (layer: number, fillStyle: string): DrawStyle => ({
+  layer,
+  fill_style: fillStyle,
+  should_fill: true,
+});
+
+const styles = {
+  treeBottom: createTreeStyle(Layer.TreeBottoms),
+  treeTop: createTreeStyle(Layer.TreeTops),
+  shadowBottom: createShadowStyle(Layer.TreeShadows, SHADOW_COLOR),
+  shadowTop: createShadowStyle(Layer.TreeTopShadows, "#eeeeef"),
 };
 
 const treeTopStyle: DrawStyle = {
- ...treeStyle,
- layer: Layer.TreeTops,
+  layer: Layer.TreeTops,
+  line_width: 0.5,
+  should_stroke: true,
+  stroke_style: "black",
 };
 
-const baseShadowStyle: DrawStyle = {
- layer: Layer.TreeShadows,
- fill_style: SHADOW_COLOR,
- should_fill: true,
+const circlePoint = (
+  theta: number,
+  center: Vector2,
+  radius: number
+): Vector2 => ({
+  x: Math.cos(theta) * radius + center.x,
+  y: Math.sin(theta) * radius + center.y,
+});
+
+const generateTreePoints = (
+  center: Vector2,
+  config: (typeof treeLayerConfigs)[number],
+  offset: boolean
+) => {
+  const { numBumps, radius, bumpRadius } = config;
+  const peakRadius = radius + bumpRadius;
+  const stepSize = (2 * Math.PI) / numBumps;
+  const halfStep = stepSize / 2;
+  let theta = offset ? stepSize / 2 : 0;
+  theta += (center.x * 31 + center.y * 17) % (2 * Math.PI);
+
+  const points: Vector2[] = [];
+  const controlPoints: Vector2[] = [];
+  const start = circlePoint(theta, center, radius);
+
+  for (let i = 1; i < numBumps; i++) {
+    theta += stepSize;
+    points.push(circlePoint(theta, center, radius));
+    controlPoints.push(circlePoint(theta - halfStep, center, peakRadius));
+  }
+
+  theta += stepSize;
+  const endControlPoint = circlePoint(theta - halfStep, center, peakRadius);
+
+  return { start, points, controlPoints, endControlPoint };
 };
 
-const topShadowStyle: DrawStyle = {
- layer: Layer.TreeTopShadows,
- fill_style: "#eeeeef",
- should_fill: true,
+const drawTreePart = (
+  drawing: Drawing,
+  style: DrawStyle,
+  {
+    start,
+    points,
+    controlPoints,
+    endControlPoint,
+  }: ReturnType<typeof generateTreePoints>,
+  bumpRadius: number,
+  offset = { x: 0, y: 0 }
+) => {
+  drawing.drawCustom(style, (ctx) => {
+    ctx.moveTo(start.x + offset.x, start.y + offset.y);
+    for (let i = 0; i < points.length; i++) {
+      ctx.arcTo(
+        controlPoints[i].x + offset.x,
+        controlPoints[i].y + offset.y,
+        points[i].x + offset.x,
+        points[i].y + offset.y,
+        bumpRadius
+      );
+    }
+    ctx.arcTo(
+      endControlPoint.x + offset.x,
+      endControlPoint.y + offset.y,
+      start.x + offset.x,
+      start.y + offset.y,
+      bumpRadius
+    );
+    ctx.closePath();
+  });
 };
-
-function circlePoint(theta: number, x: number, y: number, radius: number) {
- return {
-  x: Math.cos(theta) * radius + x,
-  y: Math.sin(theta) * radius + y,
- };
-}
 
 export function drawTree(
- drawing: Drawing,
- grid_x: number,
- grid_y: number,
- deltaTime: number
+  drawing: Drawing,
+  gridX: number,
+  gridY: number,
+  time: number
 ) {
- const pos = {
-  x: Math.round(grid_x * TILE_SIZE) + HALF_T,
-  y: Math.round(grid_y * TILE_SIZE) + HALF_T,
- };
-
- for (let i = 0; i < 2; i++) {
-  drawTreeLayer(
-   drawing,
-   pos,
-   RADIUS[i],
-   NUM_BUMPS[i],
-   BUMP_R[i],
-   i % 2 == 0,
-   i === 0,
-   deltaTime
+  const center: Vector2 = {
+    x: Math.round(gridX * TILE_SIZE) + HALF_T,
+    y: Math.round(gridY * TILE_SIZE) + HALF_T,
+  };
+  const random = center.x * 17 + center.y * 31;
+  center.x += Math.sin(random) * 6;
+  center.y += Math.cos(random) * 6;
+  const wind = (Math.sin(time + gridX + gridY) / 2 + 0.5) * 1.25;
+  const bottomPoints = generateTreePoints(center, treeLayerConfigs[0], true);
+  drawTreePart(
+    drawing,
+    styles.treeBottom,
+    bottomPoints,
+    treeLayerConfigs[0].bumpRadius,
+    { x: -wind, y: wind }
   );
- }
-}
+  drawTreePart(
+    drawing,
+    styles.shadowBottom,
+    bottomPoints,
+    treeLayerConfigs[0].bumpRadius,
+    { x: -wind - SHADOW_OFFSET, y: wind + SHADOW_OFFSET }
+  );
 
-export function drawTreeLayer(
- drawing: Drawing,
- pos: Vector2,
- radius: number,
- num_bumps: number,
- bump_r: number,
- offset: boolean,
- is_bottom: boolean,
- deltaTime: number
-) {
- const peak_r = radius + bump_r;
- const step_size = (2 * Math.PI) / num_bumps;
- const half_step = step_size / 2;
-
- let theta = offset ? step_size / 2 : 0;
-
- let points = [];
- let control_points = [];
-
- const start = circlePoint(theta, pos.x, pos.y, radius);
- for (let i = 1; i < num_bumps; i++) {
-  theta += step_size;
-  points.push(circlePoint(theta, pos.x, pos.y, radius));
-  control_points.push(circlePoint(theta - half_step, pos.x, pos.y, peak_r));
- }
- theta += step_size;
- const end_cp = circlePoint(theta - half_step, pos.x, pos.y, peak_r);
-
- const bodyStyle = is_bottom ? treeStyle : treeTopStyle;
- drawing.drawCustom(bodyStyle, (ctx) => {
-  ctx.moveTo(start.x, start.y);
-  for (let i = 0; i < points.length; i++) {
-   ctx.arcTo(
-    control_points[i].x,
-    control_points[i].y,
-    points[i].x,
-    points[i].y,
-    bump_r
-   );
-  }
-  ctx.arcTo(end_cp.x, end_cp.y, start.x, start.y, bump_r);
-  ctx.closePath();
- });
-
- const shadowStyle = is_bottom ? baseShadowStyle : topShadowStyle;
- drawing.drawCustom(shadowStyle, (ctx) => {
-  ctx.moveTo(start.x - 5, start.y + 5);
-  for (let i = 0; i < points.length; i++) {
-   ctx.arcTo(
-    control_points[i].x - 5,
-    control_points[i].y + 5,
-    points[i].x - 5,
-    points[i].y + 5,
-    bump_r
-   );
-  }
-  ctx.arcTo(end_cp.x - 5, end_cp.y + 5, start.x - 5, start.y + 5, bump_r);
-  ctx.closePath();
- });
+  const topWind = wind * 2;
+  const topPoints = generateTreePoints(center, treeLayerConfigs[1], false);
+  drawTreePart(
+    drawing,
+    styles.treeTop,
+    topPoints,
+    treeLayerConfigs[1].bumpRadius,
+    { x: -topWind, y: topWind }
+  );
+  drawTreePart(
+    drawing,
+    styles.shadowTop,
+    topPoints,
+    treeLayerConfigs[1].bumpRadius,
+    { x: -topWind - 2, y: topWind + 2 }
+  );
 }
