239eea7873a84960a4240e17e84616a89788f43d
Tyler <tfritzy@gmail.com>
2024-10-07 19:24:35 -0500

    WIP better typing system

---
+++ frontend/src/drawing/progress_bar.ts
@@ -0,0 +1,53 @@
+import { Layer } from "../constants";
+import type { Drawing, DrawStyle } from "../drawing";
+
+function drawRoundedRect(
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  radius: number
+) {
+  ctx.moveTo(x + radius, y); // Move to the top-left corner, offset by radius
+  ctx.lineTo(x + width - radius, y); // Draw a line to the top-right corner
+  ctx.quadraticCurveTo(x + width, y, x + width, y + radius); // Draw the top-right rounded corner
+  ctx.lineTo(x + width, y + height - radius); // Line down to bottom-right corner
+  ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height); // Bottom-right corner
+  ctx.lineTo(x + radius, y + height); // Line to the bottom-left corner
+  ctx.quadraticCurveTo(x, y + height, x, y + height - radius); // Bottom-left corner
+  ctx.lineTo(x, y + radius); // Line up to the top-left corner
+  ctx.quadraticCurveTo(x, y, x + radius, y); // Top-left rounded corner
+  ctx.closePath();
+}
+
+const filledPart: DrawStyle = {
+  layer: Layer.UI,
+  fill_style: "#333333",
+  should_fill: true,
+};
+const unFilledPart: DrawStyle = {
+  layer: Layer.UI,
+  stroke_style: "#333333",
+  should_stroke: true,
+  line_width: 0.5,
+};
+export function drawProgressBar(
+  drawing: Drawing,
+  x: number,
+  y: number,
+  width: number,
+  height: number,
+  radius: number,
+  progress: number
+) {
+  if (progress > 0) {
+    drawing.drawCustom(filledPart, (ctx) => {
+      drawRoundedRect(ctx, x, y, width * progress, height, radius);
+    });
+  }
+
+  drawing.drawCustom(unFilledPart, (ctx) => {
+    drawRoundedRect(ctx, x, y, width, height, radius);
+  });
+}
--- frontend/src/resource_label.ts
+++ frontend/src/resource_label.ts
@@ -1,49 +1,73 @@
 import { Layer } from "./constants";
 import type { Drawing, DrawStyle } from "./drawing";
+import { drawProgressBar } from "./drawing/progress_bar";
+import { drawWheat } from "./drawing/wheat";
 
 const style: DrawStyle = {
- layer: Layer.UI,
- fill_style: "#444444",
- font: "bold 12px Verdena",
- should_fill: true,
- text_align: "center",
+  layer: Layer.UI,
+  fill_style: "#444444",
+  font: "bold 12px Verdena",
+  should_fill: true,
+  text_align: "center",
 };
 
+const borderStyle: DrawStyle = {
+  layer: Layer.UI,
+  stroke_style: "#444444",
+  should_stroke: true,
+  line_width: 1,
+};
+
+const width = 35;
+const height = 40;
+const padding = 4;
+
 export class ResourceLabel {
- private text: string;
- private progress: number;
- private boundHandleKeyDown: (event: KeyboardEvent) => void;
- private onComplete: () => void;
- private drawing: Drawing;
+  private text: string;
+  private progress: number;
+  private boundHandleKeyDown: (event: KeyboardEvent) => void;
+  private onComplete: () => void;
+  private drawing: Drawing;
 
- constructor(text: string, drawing: Drawing, onComplete: () => void) {
-  this.text = text;
-  this.progress = 0;
-  this.drawing = drawing;
-  this.onComplete = onComplete;
-
-  this.boundHandleKeyDown = this.handleKeyDown.bind(this);
-  document.addEventListener("keydown", this.boundHandleKeyDown);
- }
-
- draw(x: number, y: number, deltaTime: number): void {
-  this.drawing.drawCustom(style, (ctx) => {
-   ctx.fillText(this.text[this.progress], x, y);
-  });
- }
-
- public removeEventListener(): void {
-  document.removeEventListener("keydown", this.boundHandleKeyDown);
- }
-
- private handleKeyDown(event: KeyboardEvent) {
-  if (this.text[this.progress] === event.key) {
-   this.progress += 1;
-
-   if (this.progress == this.text.length) {
-    this.onComplete();
+  constructor(text: string, drawing: Drawing, onComplete: () => void) {
+    this.text = text.toUpperCase();
     this.progress = 0;
-   }
+    this.drawing = drawing;
+    this.onComplete = onComplete;
+
+    this.boundHandleKeyDown = this.handleKeyDown.bind(this);
+    document.addEventListener("keydown", this.boundHandleKeyDown);
   }
- }
+
+  draw(x: number, y: number, deltaTime: number): void {
+    this.drawing.drawCustom(style, (ctx) => {
+      ctx.fillText(this.text[this.progress], x, y);
+    });
+
+    const progress = this.progress / this.text.length;
+    drawProgressBar(
+      this.drawing,
+      x - width / 2 + padding,
+      y + 8,
+      width - padding * 2,
+      5,
+      2.75,
+      progress
+    );
+  }
+
+  public removeEventListener(): void {
+    document.removeEventListener("keydown", this.boundHandleKeyDown);
+  }
+
+  private handleKeyDown(event: KeyboardEvent) {
+    if (this.text[this.progress].toLowerCase() === event.key.toLowerCase()) {
+      this.progress += 1;
+
+      if (this.progress == this.text.length) {
+        this.onComplete();
+        this.progress = 0;
+      }
+    }
+  }
 }
